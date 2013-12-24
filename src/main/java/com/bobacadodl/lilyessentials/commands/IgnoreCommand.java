9e49919d59a2f7fe4a77031823f2626ca2e5b4c6
The-Zip <1mrzip@gmail.com>
2013-12-24 10:08:12 -0800

    Create IgnoreCommand.java
    Added the ignore command.

---
+++ src/main/java/com/bobacadodl/lilyessentials/commands/IgnoreCommand.java
@@ -0,0 +1,47 @@
+package com.bobacadodl.lilyessentials.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.bobacadodl.lilyessentials.LilyEssentials;
+
+public class IgnoreCommand implements CommandExecutor 
+{
+
+        private LilyEssentials plugin;
+
+        public IgnoreCommand(LilyEssentials plugin) 
+        {
+                this.plugin = plugin;
+        }
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+        {
+
+                if (!sender.hasPermission("lilyessentials.general.message")) 
+                {
+                        return false;
+                }
+
+                // Invalid
+                if(args.length <= 0) {
+                        sender.sendMessage(plugin.prefix + ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/ignore [person]");
+                        return true;
+                }
+
+                String target = args[0];
+
+                if (plugin.getServerSync().lookupPlayer(target) == null) 
+                {
+                        sender.sendMessage(plugin.prefix + ChatColor.DARK_RED + "That player isn't around. Did you mistype their name?");
+                        return true;
+                }
+
+                // add ignore code here
+
+                return true;
+        }
+}

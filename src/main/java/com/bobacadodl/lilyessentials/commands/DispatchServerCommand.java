628c36da7127c2952ff8d2e7bbcca5577d8f1057
The-Zip <1mrzip@gmail.com>
2014-01-26 13:26:54 -0800

    Update DispatchServerCommand.java

--- src/main/java/com/bobacadodl/lilyessentials/commands/DispatchServerCommand.java
+++ src/main/java/com/bobacadodl/lilyessentials/commands/DispatchServerCommand.java
@@ -9,31 +9,36 @@ import org.bukkit.command.CommandSender;
 
 import com.bobacadodl.lilyessentials.LilyEssentials;
 
-public class DispatchServerCommand implements CommandExecutor {
+public class DispatchServerCommand implements CommandExecutor 
+{
 
 	private LilyEssentials plugin;
 
-	public DispatchServerCommand(LilyEssentials plugin) {
+	public DispatchServerCommand(LilyEssentials plugin) 
+	{
 		this.plugin = plugin;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
 
-		if(!sender.hasPermission("lilyessentials.admin.dispatchserver")) {
+		if(!sender.hasPermission("lilyessentials.admin.dispatchserver")) 
+		{
 			return false;
 		}
 
 		// Invalid
-		if(args.length <= 0) {
-			sender.sendMessage(ChatColor.DARK_RED + "Invalid args!");
-			sender.sendMessage(ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/dispatchserver [servers] [command]");
+		if(args.length <= 0) 
+		{
+			sender.sendMessage(plugin.prefix + ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/dispatchserver [servers] [command]");
 			return true;
 		}
 
 		String serverString = args[0];
 		ArrayList<String> servers = new ArrayList<String>();
-		for(String server : serverString.split(",")) {
+		for(String server : serverString.split(",")) 
+		{
 			servers.add(server);
 		}
 		String command = plugin.wordsToString(1,args);

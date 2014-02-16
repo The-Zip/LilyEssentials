fb8888fc21aeeec40f3f1445d3e00bb335ede91f
The-Zip <1mrzip@gmail.com>
2014-02-16 14:52:44 -0800

    Update DispatchServerCommand.java

--- src/main/java/com/bobacadodl/lilyessentials/commands/DispatchServerCommand.java
+++ src/main/java/com/bobacadodl/lilyessentials/commands/DispatchServerCommand.java
@@ -9,36 +9,44 @@ import org.bukkit.command.CommandSender;
 
 import com.bobacadodl.lilyessentials.LilyEssentials;
 
-public class DispatchServerCommand implements CommandExecutor{
-	LilyEssentials main;
-	public DispatchServerCommand(LilyEssentials instance){this.main=instance;}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label,
-			String[] args) {
-		
-		if(sender.hasPermission("lilyessentials.admin.dispatchserver")){
-			if(args.length>=1){
-				String serverString = args[0];
-				ArrayList<String> servers = new ArrayList<String>();
-				for(String server:serverString.split(",")){
-					servers.add(server);
-				}
-				
-				
-				String command = main.wordsToString(1,args);
+public class DispatchServerCommand implements CommandExecutor 
+{
 
-				command.replaceFirst("/", "");
-				main.request(servers, "lilyessentials.dispatch", command);
-			}
-			else{
-				//invalid
-				sender.sendMessage(ChatColor.DARK_RED+"Invalid args!");
-				sender.sendMessage(ChatColor.RED+"Proper Usage: "+ChatColor.YELLOW+"/dispatchserver [servers] [command]");
-			}
+	private LilyEssentials plugin;
+
+	public DispatchServerCommand(LilyEssentials plugin) 
+	{
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+
+		if(!sender.hasPermission("lilyessentials.admin.dispatchserver")) 
+		{
+			return false;
+		}
+
+		// Invalid
+		if(args.length <= 0) 
+		{
+			sender.sendMessage(plugin.prefix + ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/dispatchserver [servers] [command]");
 			return true;
 		}
-		
-		return false;
+
+		String serverString = args[0];
+		ArrayList<String> servers = new ArrayList<String>();
+		for(String server : serverString.split(",")) 
+		{
+			servers.add(server);
+		}
+		String command = plugin.wordsToString(1,args);
+
+		command = command.substring(1);
+		plugin.request(servers, "lilyessentials.dispatch", command);
+
+		return true;
+
 	}
 }

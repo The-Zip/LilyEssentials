889cdf079ce0546cacf23671fdd03c8063b8898a
The-Zip <1mrzip@gmail.com>
2014-05-02 18:10:47 -0700

    Messages

--- src/main/java/com/bobacadodl/lilyessentials/commands/MessageCommand.java
+++ src/main/java/com/bobacadodl/lilyessentials/commands/MessageCommand.java
@@ -7,37 +7,48 @@ import org.bukkit.command.CommandSender;
 
 import com.bobacadodl.lilyessentials.LilyEssentials;
 
-public class MessageCommand implements CommandExecutor {
+public class MessageCommand implements CommandExecutor 
+{
 
 	private LilyEssentials plugin;
 
-	public MessageCommand(LilyEssentials plugin) {
+	public MessageCommand(LilyEssentials plugin) 
+	{
 		this.plugin = plugin;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
 
-		if (!sender.hasPermission("lilyessentials.general.message")) {
+		if (!sender.hasPermission("lilyessentials.general.message")) 
+		{
 			return false;
 		}
 
 		// Invalid
-		if (args.length <= 1) {
-			sender.sendMessage(ChatColor.DARK_RED + "Invalid args!");
-			sender.sendMessage(ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/msg [player] [message]");
+		if (args.length <= 1) 
+		{
+			sender.sendMessage(plugin.prefix + ChatColor.RED + "Proper Usage: " + ChatColor.YELLOW + "/msg [player] [message]");
 			return true;
 		}
 
 		String target = args[0];
 
-		if (plugin.getServerSync().lookupPlayer(target) == null) {
-			sender.sendMessage(ChatColor.DARK_RED + "Error!");
-			sender.sendMessage(ChatColor.RED + "Player is offline");
+		if (plugin.getServerSync().lookupPlayer(target) == null) 
+		{
+			sender.sendMessage(plugin.prefix + ChatColor.DARK_RED + "This player is not online. Did you spell it right?");
 			return true;
-		}
+		}		
 
-		String message = plugin.wordsToString(1, args);
+		//String message = plugin.wordsToString(1, args);
+		String message = "";
+        
+        for(int i = 1; i < args.length; i++)
+        {
+        	message += (i == args.length - 1) ? args[i] : args[i] + " ";
+        }
+        
 		plugin.request("lilyessentials.message", target + "\0"
 				+ sender.getName() + "\0" + message + "\0"
 				+ plugin.getUsername());
